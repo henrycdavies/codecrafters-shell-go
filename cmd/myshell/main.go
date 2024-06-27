d7340d8d1d7774f52c471007ebef50b0211afd39
Henry Davies <henry.davies@bp.com>
2024-06-27 09:11:28 +0100

    implement exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,15 +13,57 @@ func handleCommandNotFound(command string) {
 	fmt.Fprintf(os.Stdout, "%s: command not found", command)
 }
 
+func exit(args []string) (error) {
+	if (len(args) != 1) {
+		errMsg := "an exit code arg must be provided to exit command"
+		fmt.Fprintf(os.Stdout, "%s", errMsg)
+		return errors.New(errMsg)
+	}
+	code, err := strconv.Atoi(args[0])
+	if (err != nil) {
+		errMsg := "an exit code arg must be provided to exit command"
+		fmt.Fprintf(os.Stdout, "%s", errMsg)
+		return errors.New(errMsg)
+	}
+	os.Exit(code)
+	return nil
+}
+
+func handleCommand(command string, args []string) {
+	switch command {
+	case "exit":
+		exit(args)
+	default:
+		handleCommandNotFound(command)
+	}
+}
+
+func parseInput(input string) (string, []string, error) {
+	trimmed := strings.TrimSpace(input)
+	parts := strings.Split(trimmed, " ")
+	if (len(parts) == 0) {
+		return "", nil, errors.New("no input received")
+	}
+	command := parts[0]
+	var args []string
+	if (len(parts) == 1) {
+		args = nil
+	} else {
+		args = parts[1:]
+	}
+	return command, args, nil
+}
+
 func handleInput() {
-	fmt.Fprint(os.Stdout, "$ ")
+	fmt.Fprint(os.Stdout, "\n$ ")
 	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if (err != nil) {
 		fmt.Fprintf(os.Stdout, "%s", err)
 	}
-	command := strings.TrimSpace(input)
-	handleCommandNotFound(command)
-	fmt.Fprint(os.Stdout, "\n")
+	command, args, err := parseInput(input)
+	if (err == nil) {
+		handleCommand(command, args)
+	}
 	handleInput()
 }
 
